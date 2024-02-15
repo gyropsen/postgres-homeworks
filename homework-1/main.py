e40afcfcc3ba3f098c27c19ae02d5af8bac8be63
egor <sharapov.egor00@gmail.com>
2024-02-15 18:58:11 +0300

    homework1 done

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,38 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+from pathlib import Path
+
+source = Path(Path(__file__).parent.parent, "north_data")
+
+
+def get_tuples(filename) -> list[tuple]:
+    lst = []
+    with open(Path(source, filename), 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)
+        for read in csvreader:
+            lst.append(tuple(read))
+    return lst
+
+
+customers = get_tuples("customers_data.csv")
+employees = get_tuples("employees_data.csv")
+orders = get_tuples("orders_data.csv")
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="12345"
+)
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            cur.executemany("INSERT INTO customers VALUES (%s, %s, %s)", customers)
+            cur.executemany("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", employees)
+            cur.executemany("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", orders)
+
+finally:
+    conn.close()
